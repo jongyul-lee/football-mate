511b8911fc472a4c8d53f34e2225740bec0a742c
jongyul-lee <dlwhddbf0614@gmail.com>
2026-05-24 20:49:01 +0900

    init project

---
+++ src/main/java/jylee/football_mate/FootballMateApplication.java
@@ -0,0 +1,13 @@
+package jylee.football_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jylee/football_mate/FootballMateApplicationTests.java
@@ -0,0 +1,13 @@
+package jylee.football_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
